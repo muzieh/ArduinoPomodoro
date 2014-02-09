14f5bc0d58eddd2e6a07901163d637d568f28255
Muzieh <muzieh@gmail.com>
2014-02-09 22:48:14 +0000

    clean up, refactoring

--- ArduinoPomodoro.ino
+++ ArduinoPomodoro.ino
@@ -1,9 +1,8 @@
 #include <LiquidCrystal.h>
-
+#include <stdio.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 long counterTime = 60 * 60 * 1000L; 
-long minutes = 0;
 long startTime =0;
 int buttonWorkPin = 8;
 int buttonBreakPin = 9;
@@ -23,32 +22,32 @@ void setup() {
   pinMode(buttonBreakPin, INPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("idle...");
-  lcd.setCursor(0,1);
-  lcd.print("ready to work?");
+//  lcd.print("idle...");
+//  lcd.setCursor(0,1);
+//  lcd.print("ready to work?");
 }
 
 int currentMillis = 0;
+/*
 void updateTime() {
   switch(currentStatus) {
     case STATUS_WORK:
     case STATUS_BREAK:
     case STATUS_LONG_BREAK:
-      
       break;
   }
   currentMillis = millis();
 
   //previousMillis = currentMillis();
 }
-
+*/
 void startCounting(int countType) {
  switch(countType) {
     case STATUS_WORK:
       counterTime = 25 * 60 * 1000L;
       break;
     case STATUS_BREAK:
-      counterTime = 5 * 60 * 1000L;
+      counterTime = 1 * 10 * 1000L;
       break;
     case STATUS_LONG_BREAK:
       counterTime = 20 * 60 * 1000L;
@@ -59,12 +58,8 @@ void startCounting(int countType) {
   startTime = millis();
 }
 
-void printTime() {    
-  lcd.clear();
-  
-  long timeFromStart = millis() - startTime;
-  long remaining = counterTime - timeFromStart;  
-  switch(currentStatus) {
+void printHeaderLine() {
+   switch(currentStatus) {
     case STATUS_STOP:
       lcd.print("idle...");
       lcd.setCursor(0,1);
@@ -80,61 +75,63 @@ void printTime() {
       lcd.print("LBreak");
       break;
   }
-  lcd.print(remaining);
-  lcd.setCursor(0,1);
-  
-  int innerStart = remaining / 1000;
-  if(remaining < 0 )
-  {
-    innerStart = abs(remaining) / 1000;
-  }
-  minutes = innerStart / 60;
-  if(minutes < 10 )
-    lcd.print("0");
-  if(remaining < 0) {
-    lcd.print("-");
-  }
-  lcd.print(minutes);
-  lcd.print(":");
-  int seconds = innerStart - minutes * 60;
-  if(seconds < 10)
-    lcd.print("0");
-  lcd.print(seconds);
 }
 
-int st= 0;
-int currentState(int pin) {
- st = digitalRead(pin);
+void printTime() {    
+  lcd.clear();
+  
+  long timeFromStart = millis() - startTime;
+  long remaining = counterTime - timeFromStart;
+ 
+  printHeaderLine();  
+  if(currentStatus == STATUS_STOP)
+    return;
+    
+  //lcd.print(remaining);
+  lcd.setCursor(0,1);
+    
+  int innerStart = abs(remaining) / 1000;
+  if(remaining < 0 )
+  {
+    lcd.print("-");
+  }
+
+  int minutes = innerStart / 60;
+  int seconds = innerStart - minutes * 60;
+
+  char lineBuffer[16];
+  sprintf(lineBuffer,"%02d:%02d", minutes, seconds);
+  lcd.write(lineBuffer);
+}
+
+int getButtonState(int pin) {
+ int st = digitalRead(pin);
  delay(10);
  if(st == digitalRead(pin))
- {
     return st;
- }
- else
- {
-   return 0;
- }
+    
+ return 0;
 }
 
 
 void loop() {
  
-  updateTime();
+ // updateTime();
   printTime();
 
-  int cs = currentState(buttonWorkPin);
-  if(cs == 1 && previousWorkButtonState == 0)
+  int buttonState = getButtonState(buttonWorkPin);
+  if(buttonState == 1 && previousWorkButtonState == 0)
   {
     startCounting(STATUS_WORK);
   }
-  previousWorkButtonState = cs;
+  previousWorkButtonState = buttonState;
   
-  cs = currentState(buttonBreakPin);
-  if(cs == 1 && previousBreakButtonState == 0)
+  buttonState = getButtonState(buttonBreakPin);
+  if(buttonState == 1 && previousBreakButtonState == 0)
   {
     startCounting(STATUS_BREAK);
   }
-  previousBreakButtonState = cs;
+  previousBreakButtonState = buttonState;
   delay(50);
   Serial.print(digitalRead(buttonWorkPin));
   Serial.write(" ");
